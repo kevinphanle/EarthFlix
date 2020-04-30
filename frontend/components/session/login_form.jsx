55c3c7ad54f1dcb8b31ecdd5a8f03fe8ad69a23b
Kevin Le <kevinphanle11@gmail.com>
2020-04-29 21:45:39 -0700

    fixed login page background image

--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -1,105 +1,117 @@
 import React from "react";
-import { Link, withRouter } from 'react-router-dom';
-import * as Images from '../images';
+import { Link, withRouter } from "react-router-dom";
+import * as Images from "../images";
 
 class LoginForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            password: ""
-        }
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleGuestLogin = this.handleGuestLogin.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleGuestLogin = this.handleGuestLogin.bind(this);
+  }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        const { email, password } = this.state;
-        const user = { email, password };
-        this.props.login(user).then(() => this.props.history.push('/browse'))
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    const { email, password } = this.state;
+    const user = { email, password };
+    this.props.login(user).then(() => this.props.history.push("/browse"));
+  }
 
-    initLogin(event) {
-        event.preventDefault();
-        const email = "guest@guest.com";
-        const password = "password";
-    
-        email.split("").forEach((char, emailIndex) => {
+  initLogin(event) {
+    event.preventDefault();
+    const email = "guest@guest.com";
+    const password = "password";
+
+    email.split("").forEach((char, emailIndex) => {
+      setTimeout(() => {
+        this.setState({ email: this.state.email + char });
+      }, emailIndex * 50);
+
+      if (emailIndex === email.length - 1)
+        password.split("").forEach((char, passwordIndex) => {
           setTimeout(() => {
-            this.setState({ email: this.state.email + char });
-          }, emailIndex * 50);
-    
-          if (emailIndex === email.length - 1)
-            password.split("").forEach((char, passwordIndex) => {
-              setTimeout(() => {
-                this.setState({ password: this.state.password + char }, () => {
-                  if (passwordIndex === password.length - 1) this.props.login(this.state);
-                });
-              }, passwordIndex * 50 + emailIndex * 50);
+            this.setState({ password: this.state.password + char }, () => {
+              if (passwordIndex === password.length - 1)
+                this.props.login(this.state);
             });
+          }, passwordIndex * 50 + emailIndex * 50);
         });
-      }
+    });
+  }
 
-    handleGuestLogin(e) {
-        e.preventDefault();
-        this.initLogin(e);
-        // this.props.login(this.state).then(() => this.props.history.push('/browse'));
-    }
+  handleGuestLogin(e) {
+    e.preventDefault();
+    this.initLogin(e);
+    // this.props.login(this.state).then(() => this.props.history.push('/browse'));
+  }
 
+  update(field) {
+    return (e) => {
+      this.setState({ [field]: e.target.value });
+    };
+  }
 
-    update(field) {
-        return (e) => {
-            this.setState({ [field]: e.target.value})
-        }
-    }
+    render() {
+      
+        console.log(Images.earthBg)
+    return (
+      <div className="login-page" style={{backgroundImage: `url(${Images.earthBg})` }}>
+        {/* <div className="login-wrapper-background">
+          <img src={Images.earthBg} alt="" />
+        </div> */}
 
-    render () {
-        return (
+        <section className="login-form-wrapper">
+          <div className="login-form-body">
+            <h3>{this.props.formType}</h3>
 
-            <div>
-                <div className="login-wrapper-background">
-                    <img src={Images.earthBg} alt=""/>
-                </div>
+            <form className="login-form" onSubmit={this.handleSubmit}>
+              <label htmlFor="email">
+                <span>Email:</span>
+                <input
+                  type="text"
+                  id="email"
+                  className="login-email-input"
+                  value={this.state.email}
+                  onChange={this.update("email")}
+                />
+              </label>
+              <label htmlFor="password">
+                <span>Password:</span>
+                <input
+                  type="password"
+                  id="password"
+                  className="login-password-input"
+                  value={this.state.password}
+                  onChange={this.update("password")}
+                />
+              </label>
 
-                <section className="login-form-wrapper">
-                    <div className="login-form-body">
-                        <h3>{this.props.formType}</h3>
+              <input
+                type="submit"
+                className="login-form-btn"
+                value={this.props.formType}
+              />
 
-                        <form className="login-form" onSubmit={this.handleSubmit}>
-                            <label htmlFor="email">
-                                <span>Email:</span>
-                                <input
-                                    type="text"
-                                    id="email"
-                                    className="login-email-input"
-                                    value={this.state.email}
-                                    onChange={this.update('email')}
-                                />
-                            </label>
-                            <label htmlFor="password">
-                                <span>Password:</span>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    className="login-password-input"
-                                    value={this.state.password}
-                                    onChange={this.update('password')}
-                                />
-                            </label>
+              <input
+                type="submit"
+                className="guest-login-btn"
+                value="Demo User Login"
+                onClick={this.handleGuestLogin}
+              />
 
-                            <input type="submit" className="login-form-btn" value={this.props.formType} />
+              <p className="other-form">
+                New to EarthFlix? <Link to="/signup">Sign Up now!</Link>
+              </p>
+            </form>
 
-                            <input type="submit" className="guest-login-btn" value="Demo User Login" onClick={this.handleGuestLogin}/>
-
-                            <p className="other-form">New to EarthFlix? <Link to="/signup">Sign Up now!</Link></p>
-                        </form>
-
-                        <figure className="black-bg"></figure>
-                    </div>
-                </section>
-            </div>
-        );
-    }
+            <figure className="black-bg"></figure>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 export default withRouter(LoginForm);
